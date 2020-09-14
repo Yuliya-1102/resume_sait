48b24fcbd9a0a71c12e5ab1f265676096b59a8db
unknown <bamdina-kissi@yandex.ru>
2020-09-14 13:01:44 +0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -10,5 +10,25 @@ $(function(){
   	
 });
 
+const btns = document.querySelectorAll('.hero-btn');
+const modalOverlay = document.querySelector(".modal-overlay");
+const modalWindow = document.querySelectorAll('.modal-window');
 
-console.log('Привет');
+btns.forEach((el) => {
+	el.addEventListener('click', (event) => {
+		let path = event.currentTarget.getAttribute('data-path')
+		modalOverlay.classList.add('modal-overlay--visible') 
+		document.querySelector(`[data-target="${path}"]`).classList.add('modal-window--add')
+	})
+})
+
+modalOverlay.addEventListener('click', (e) => {
+	if(e.target == modalOverlay){
+		modalOverlay.classList.remove('modal-overlay--visible')
+		modalWindow.forEach((el) => {
+			el.classList.remove('modal-window--add')
+		})
+	}
+	
+	
+})
